eff43c57f6a334b5f9c04e94dbc751388a41e197
Alex Telegin <telegin.alexander@gmail.com>
2022-03-05 20:36:50 +0400

    Remove package-lock, deploy script wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
-// const path = require('path')
-// require('dotenv').config({ path: path.resolve(__dirname, '/.env') })
+require("dotenv").config()
 
-// const { PRIVATEKEY } = process.env;
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const { PRIVATEKEY } = process.env;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -26,13 +12,11 @@ module.exports = {
   networks: {
     hardhat: {
     },
-    /*
     mumbai: {
       networkId: 80001,
       url: "https://rpc-mumbai.matic.today",
       accounts: [PRIVATEKEY]
     }
-    */
   },
   solidity: {
     version: "0.8.12",
